490a3b5c0668fd5c86c55a7745f3765cce430b94
ch33zer <ch33zer@gmail.com>
2013-01-19 00:13:32 -0500

    Fixing jed

--- smang/smang.js
+++ smang/smang.js
@@ -17,3 +17,19 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+		var Dragbox = function(str) {
+			ret = document.createElement("div");
+			ret.setAttribute("class", "draggable");
+			anc = document.createElement("a");
+			anc.setAttribute("href", str);
+			anc.innerHTML = str;
+			ret.appendChild(anc);;
+			return ret;
+		}
+		
+		function addDraggable(target, drag) {
+			target.appendChild(drag);
+			$(function() {
+				$(".draggable").draggable({containment:'parent'});
+			});
+		}
