fa5c28ae1d57f3e01d05710ed2f7ef238a4ab111
ch33zer <ch33zer@gmail.com>
2013-01-19 01:41:27 -0500

    Lets try this again...

--- smang/smang.js
+++ smang/smang.js
@@ -1,35 +1,72 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to smang.";
-  };
+Links = new Meteor.Collection("links");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
+	Template.link.dragtext = function() {
+		return Links.findOne(this._id).urltext;	
+	}
+	Template.display.links = function() {
+		return Links.find({},{sort: {urltext:1, x: -1, y: -1}});
+	}
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Links.find().count() === 0) {
+      var urls = ["http://jhu.edu",
+                   "http://youtube.com"];
+      for (var i = 0; i < urls.length; i++)
+        Links.insert({urltext: urls[i], x: Math.floor(Math.random()*10)*5, y: Math.floor(Math.random()*10)*5});
+    
+
+	}
   });
 }
-		var Dragbox = function(str) {
-			ret = document.createElement("div");
-			ret.setAttribute("class", "draggable");
-			anc = document.createElement("a");
-			anc.setAttribute("href", str);
-			anc.innerHTML = str;
-			ret.appendChild(anc);;
-			return ret;
-		}
-		
-		function addDraggable(target, drag) {
-			target.appendChild(drag);
-			$(function() {
-				$(".draggable").draggable({containment:'parent'});
-			});
-		}
+/*
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Players = new Meteor.Collection("players");
+
+if (Meteor.isClient) {
+  Template.leaderboard.players = function () {
+    return Players.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
+  };
+
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "selected" : '';
+  };
+
+  Template.leaderboard.events({
+    'click input.inc': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selected_player", this._id);
+    }
+  });
+}
+
+// On server startup, create some players if the database is empty.
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Players.find().count() === 0) {
+      var names = ["Ada Lovelace",
+                   "Grace Hopper",
+                   "Marie Curie",
+                   "Carl Friedrich Gauss",
+                   "Nikola Tesla",
+                   "Claude Shannon"];
+      for (var i = 0; i < names.length; i++)
+        Players.insert({name: names[i], score: Math.floor(Math.random()*10)*5});
+    }
+  });
+}
+*/
